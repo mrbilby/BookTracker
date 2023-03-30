5313f4b420762997b2888aabba6e06d3171bec05
mrbilby <baileymrb@gmail.com>
2023-03-30 18:22:23 +0100

    Initial Commit

---
+++ BookTracker/BookTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookTrackerApp.swift
+//  BookTracker
+//
+//  Created by James Bailey on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
